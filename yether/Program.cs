4e86392a921b201267b73b02382bb26003d73b90
Buse <Buse@DESKTOP-7EVT6O5>
2021-12-16 14:21:09 +0100

    Proje dosyası ekle.

---
+++ yether/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace yether
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a number: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int i = 2, j;
+            int sayac;
+            while (i < sayi)
+            {
+                sayac = 0;
+                j = 2;
+                while (j < i)
+                {
+                    if (i % j == 0)
+                    {
+                        sayac++;
+                    }
+                    j++;
+                }
+                if (sayac == 0)
+                {
+                    Console.WriteLine("Prime Numbers = " + i);
+                }
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
